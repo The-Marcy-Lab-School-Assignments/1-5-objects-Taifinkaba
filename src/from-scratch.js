6cdb53a991b4bcd78ebe6d9277f4f0c0cdc0fd59
Taifinkaba <taifinkabad@gmail.com>
2024-04-11 15:11:22 -0400

    Q1 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,19 @@
-const coolGreeting = () => {
+const coolGreeting = (person, isCool) => {
+  person = {
+    name: 'Sara',
+    bio: 'Too cool 4 skool.',
+    age: 32,
+    isCool: true
+  }
+  let peep = person.name.toUpperCase();
+  if(isCool === true){
+    return `What is UP ${peep}? How you been doin'?`;
+  }else{
+    return `Greetings ${person.name}, how have you been lately?`;
+  }
 };
+// console.log(coolGreeting());
+
 
 const haveBirthday = () => {
 };
